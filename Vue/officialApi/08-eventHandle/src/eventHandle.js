77435a3c0b10242ad8f2cae0042a74c0fdda8299
wqhash <wqhash@163.com>
2018-02-26 23:02:06 +0800

    add diff

--- Vue/officialApi/08-eventHandle/src/eventHandle.js
+++ Vue/officialApi/08-eventHandle/src/eventHandle.js
@@ -4,7 +4,7 @@ var vm = new Vue({
   data: data,
   methods: {
     greet(arg) {
-      console.log(event)
+      console.log(event);
       console.log('js文件中手动执行的方法!!!' + arg);
       if (event) {
         alert(event.target.tagName);
